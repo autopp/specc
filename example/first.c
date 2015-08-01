ce002a5cd4019c77b38baea66ed98a26b9616691
autopp <autopp.inc@gmail.com>
2015-08-01 22:18:57 +0900

    add most small example

--- example/first.c
+++ example/first.c
@@ -1,5 +1,16 @@
 #include <specc.h>
 #include <stdio.h>
 
+int inc(int x){
+  return x + 1;
+}
+
 specc_main {
+  describe("inc"){
+    describe("with 41"){
+      it("returns 42"){
+        expect_that(inc(41) == 42);
+      }
+    }
+  }
 }
