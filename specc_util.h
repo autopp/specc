4580435d55f476c582e768bdf321219d2f7419fd
autopp <autopp.inc@gmail.com>
2015-10-12 14:40:27 +0900

    add doccomment for utility code

--- specc_util.h
+++ specc_util.h
@@ -4,16 +4,29 @@
 #include <stdio.h>
 #include <unistd.h>
 
+/**
+ * Get second
+ * @return second
+ */
 double specc_get_time();
 
 /**
- * output formatted error message and exit program
- * @param fmt printf style format string
+ * Output formatted error message and exit program
+ * @param fmt Printf style format string
  */
 void specc_internal_error(const char *fmt, ...);
 
+/**
+ * Output syntax error message and exit program
+ * @param filename File name syntax error has occurred
+ * @param line     Line number syntax error has occurred
+ * @param fmt      Printf style format string
+ */
 void specc_syntax_error(const char *filename, int line, const char *fmt, ...);
 
+/**
+ * Set of color ID
+ */
 typedef enum specc_Color {
   specc_DEFAULT_COLOR,
   specc_RED,
@@ -24,53 +37,100 @@ typedef enum specc_Color {
 } specc_Color;
 
 /**
- * format print for stdout with specifieid indent
+ * Formatted print for fp with specifieid indent and color
+ * @param fp    Output target
  * @param level indenet level
+ * @param color Output color ID (Only when fp points tty)
  * @param fmt   printf style format string
  */
 void specc_fcprintf_indented(FILE *fp, specc_Color color, int level, const char *fmt, ...);
 
+/**
+ * Almost the same as specc_fcprintf_indented, but to output a newline
+ * @param fp    Output target
+ * @param level indenet level
+ * @param color Output color ID (Only when fp points tty)
+ */
 #define specc_fcprintfln_indented(fp, color, level, ...)\
   do {\
     specc_fcprintf_indented((fp), (color), (level), __VA_ARGS__);\
     fprintf((fp), "\n");\
   } while (0)
 
+/**
+ * Formatted print for fp with specifieid indent and color
+ * @param level indenet level
+ * @param color Output color ID (Only when fp points tty)
+ */
 #define specc_cprintf_indented(color, level, ...)\
   specc_fcprintf_indented(stdout, (color), (level), __VA_ARGS__)
 
+/**
+ * Almost the same as specc_cprintf_indented, but to output a newline
+ * @param level indenet level
+ * @param color Output color ID (Only when fp points tty)
+ */
 #define specc_cprintfln_indented(color, level, ...)\
   specc_fcprintfln_indented(stdout, (color), (level), __VA_ARGS__)
 
+
+/**
+ * Formatted print for stdout with specifieid indent
+ * @param level indenet level
+ */
 #define specc_printf_indented(level, ...)\
   specc_cprintf_indented(specc_DEFAULT_COLOR, level, __VA_ARGS__)
 
+/**
+ * Almost the same as specc_printf_indented, but to output a newline
+ * @param level indenet level
+ * @param color Output color ID (Only when fp points tty)
+ */
 #define specc_printfln_indented(level, ...)\
   specc_cprintfln_indented(specc_DEFAULT_COLOR, level, __VA_ARGS__)
 
+/**
+ * Formatted print for stdout
+ */
 #define specc_printf(...) specc_printf_indented(0, __VA_ARGS__)
 
+/**
+ * Almost the same as specc_printf, but to output a newline
+ */
 #define specc_printfln(...)\
   specc_printfln_indented(0, __VA_ARGS__)
 
+/**
+ * Formatted print for stdout with specifieid color
+ * @param color Output color ID
+ */
 #define specc_cprintf(color, ...)\
   specc_cprintf_indented((color), 0, __VA_ARGS__)
 
+/**
+ * Almost the same as specc_cprintf, but to output a newline
+ * @param color Output color ID
+ */
 #define specc_cprintfln(color, ...)\
   specc_cprintfln_indented((color), 0, __VA_ARGS__)
 
 /**
- * print newline
+ * Print newline
  */
 #define specc_newline() (putchar('\n'))
 
 /**
- * allocate and format string
+ * Allocate and format string
  * @param fmt printf style format string
- * @return new string
+ * @return new string (This string needs to be freed at end)
  */
 const char *specc_saprintf(const char *fmt, ...);
 
+/**
+ * Return Name of signal number (If not signal number, return NULL)
+ * @param  signum Signal number
+ * @return        Name of signal or NULL (This string should NOT be freed)
+ */
 const char *specc_signal_name(int signum);
 
 #endif
